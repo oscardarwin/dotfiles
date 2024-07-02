661f3635c93416430bafa91c12fa0f10838ade28
hallayus <hallayus@gmail.com>
2024-07-02 23:33:29 +0200

    adding reaper

--- flake.nix
+++ flake.nix
@@ -106,6 +106,7 @@
           ./modules/nixos/screensharing.nix
           ./modules/nixos/rclone.nix
           ./modules/nixos/resource-monitoring.nix
+          ./modules/nixos/reaper.nix
           ./modules/nixos/chat.nix
           ./hardware/squirtle.nix
           inputs.nixos-hardware.nixosModules.microsoft-surface-laptop-amd
