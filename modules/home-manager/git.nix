57290af106690dac185983b3d4a4020e4e90c756
Hallayus <hallayus@gmail.com>
2024-09-11 21:10:56 +0200

    updating git config to not require signing

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -2,13 +2,8 @@
   programs = {
     git = {
       enable = true;
-      includes = [{ path = "~/.config/git/user"; }];
-      extraConfig = { gpg.format = "ssh"; };
-      signing = {
-        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBXwNal8WbxW4K7noFVTiKKFwNBMvAh7BsbmISMj1q41";
-        signByDefault = true;
-        gpgPath = "/opt/1Password/op-ssh-sign";
-      };
+      userEmail = "hallayus@gmail.com";
+      userName = "Hallayus";
     };
     lazygit.enable = true;
   };
