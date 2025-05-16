4160a881d33fb0e63075ae12e1e20052daf417b3
Hallayus <hallayus@gmail.com>
2025-05-16 20:50:27 +0200

    pull 1password sessions as environment variables running in my apps

--- modules/home-manager/window-manager/execute_in_workspace.sh
+++ modules/home-manager/window-manager/execute_in_workspace.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+
+SESSION_1PASSWORD_ENV_VAR=$(systemctl --user show-environment | grep ^OP_SESSION_ | head -n1)
+
 WORKSPACE_NAME=$2
 TO_EXECUTE=$1
 WORKSPACE_EXISTS=$(exec swaymsg -t get_workspaces | grep '"name": "'$WORKSPACE_NAME'"')
 if [ -n "$WORKSPACE_EXISTS" ]; then exec swaymsg "workspace $WORKSPACE_NAME"
-else swaymsg "workspace $WORKSPACE_NAME; exec $TO_EXECUTE"
+else swaymsg "workspace $WORKSPACE_NAME; exec systemd-run --user --scope --quiet --setenv $SESSION_1PASSWORD_ENV_VAR $TO_EXECUTE"
 fi
