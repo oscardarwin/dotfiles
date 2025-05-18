b42cd6f62ab209e286c8f30c36ccae2d0af5defd
Hallayus <hallayus@gmail.com>
2025-05-18 14:14:46 +0200

    incorrect argument names

--- modules/home-manager/hotspot.sh
+++ modules/home-manager/hotspot.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 OP_SESSION=$(systemctl --user show-environment | grep '^OP_SESSION=' | cut -d= -f2-)
-SSID=$(op item get hotspot --field=username --token "$OP_SESSION")
-PASSWORD=$(op item get hotspot --field=password --token "$OP_SESSION")
+SSID=$(op item get hotspot --field=username --session "$OP_SESSION")
+PASSWORD=$(op item get hotspot --field=password --session "$OP_SESSION" --reveal)
 
 if [ -n "$SSID" ] && [ -n "$PASSWORD" ]; then
   nmcli device wifi connect "$SSID" password "$PASSWORD"
