69c2f4c0e58278518d58c98bfb4605625da5d07f
Hallayus <hallayus@gmail.com>
2025-06-15 21:22:06 +0200

    remove musescore add zathura for pdfs

--- modules/home-manager/packages.nix
+++ modules/home-manager/packages.nix
@@ -1,12 +1,11 @@
 { pkgs, ... }: {
   home.packages = [
     pkgs.libresprite
-    pkgs.musescore
-    pkgs.muse-sounds-manager
     pkgs.rclone
     pkgs.lilypond
     pkgs.fluidsynth
     pkgs.ffmpeg
     pkgs.soundfont-fluid
+    pkgs.zathura
   ];
 }
