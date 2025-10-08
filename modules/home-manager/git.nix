3994018aaeadbcc6349a25b1766f0d52b4d1c791
oscar.darwin@understand.ai <oscar.darwin@understand.ai>
2025-10-08 16:51:28 +0200

    feat: add conventional commits to lazygit config

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,10 +1,102 @@
 { ... }: {
   programs = {
-    lazygit.enable = true;
     git = {
       enable = true;
       userName = "oscardarwin";
       userEmail = "6627886+oscardarwin@users.noreply.github.com";
+    };
+    lazygit = {
+      enable = true;
+      settings = {
+        customCommands = [{
+          key = "c";
+          context = "files";
+          prompts = [
+            {
+              type = "menu";
+              title = "semantic context?";
+              key = "prefix";
+              options = [
+                {
+                  name = "feat";
+                  description = "increase minor version";
+                  value = "feat";
+                }
+                {
+                  name = "fix";
+                  description = "increase bugfix version";
+                  value = "fix";
+                }
+                {
+                  name = "chore";
+                  description = "hidden change";
+                  value = "chore";
+                }
+                {
+                  name = "build";
+                  description = "change affecting build";
+                  value = "build";
+                }
+                {
+                  name = "ci";
+                  description = "change affecting ci";
+                  value = "ci";
+                }
+                {
+                  name = "docs";
+                  description = "change affecting docs";
+                  value = "docs";
+                }
+                {
+                  name = "style";
+                  description = "change affecting styling";
+                  value = "style";
+                }
+                {
+                  name = "refactor";
+                  description = "change affecting refactoring";
+                  value = "refactor";
+                }
+                {
+                  name = "perf";
+                  description = "change affecting performance";
+                  value = "perf";
+                }
+                {
+                  name = "test";
+                  description = "change affecting tests";
+                  value = "test";
+                }
+              ];
+            }
+            {
+              type = "menu";
+              title = "breaking change?";
+              key = "breaking";
+              options = [
+                {
+                  name = "no breaking change";
+                  description = "this is a normal change";
+                  value = "";
+                }
+                {
+                  name = "breaking change";
+                  description = "this is a breaking change";
+                  value = "!";
+                }
+              ];
+            }
+            {
+              type = "input";
+              title = "commit message";
+              key = "commitMessage";
+              initialValue = "";
+            }
+          ];
+          command = "git commit -m '{{.Form.prefix}}{{.Form.breaking}}: {{.Form.commitMessage}}'";
+          loadingText = "creating commit";
+        }];
+      };
     };
   };
 
