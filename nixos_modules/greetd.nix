50356c121446c8f4ca58c1841634d23bc18a62b0
oscardarwin <6627886+oscardarwin@users.noreply.github.com>
2026-02-12 17:48:57 +0100

    feat: add greeter user

--- nixos_modules/greetd.nix
+++ nixos_modules/greetd.nix
@@ -5,8 +5,11 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --cmd ${pkgs.sway}/bin/sway";
-        user = "oscar";
+        command = "${pkgs.tuigreet}/bin/tuigreet --user-menu --greet-align left --cmd ${pkgs.sway}/bin/sway";
+        user = "greeter";
+      };
+      terminal = {
+        vt = 1;
       };
     };
     useTextGreeter = true;
@@ -14,4 +17,15 @@
 
   services.dbus.enable = true;
   security.polkit.enable = true;
+
+  users.users.greeter = {
+    isSystemUser = true;
+    description = "Greeter user";
+    home = "/var/lib/greeter";
+    createHome = true;
+    group = "greeter";
+    extraGroups = [ "video" "input" ];
+  };
+
+  users.groups.greeter = { };
 }
