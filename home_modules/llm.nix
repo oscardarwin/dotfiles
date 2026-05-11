8f4f493746f5f41f391f2567b0d8e81a58656eda
oscardarwin <6627886+oscardarwin@users.noreply.github.com>
2026-05-11 10:49:58 +0200

    fix: try autogenerating providers for Avante

--- home_modules/llm.nix
+++ home_modules/llm.nix
@@ -1,27 +1,41 @@
-{ config, ... }:
+{ config, lib, ... }:
+
 let
   dummyLiteLLMApiKey = "DUMMY";
+
   port = toString config.my.litellm.port;
-  model = config.my.ollama.default_model;
+
+  mkName = model: lib.replaceStrings [ ":" "." "-" ] [ "_" "_" "_" ] model;
+
+  mkProvider = model: {
+    name = mkName model;
+
+    value = {
+      model = model;
+
+      __inherited_from = "openai";
+
+      endpoint = "http://localhost:${port}/v1";
+
+      api_key_name = dummyLiteLLMApiKey;
+    };
+  };
+
+  providers =
+    builtins.listToAttrs
+      (map mkProvider config.my.litellm.models);
+
+  defaultProvider = mkName (builtins.elemAt config.my.litellm.models 0);
+
 in
 {
-  programs.nixvim = {
-    plugins = {
-      avante = {
-        enable = true;
-        settings = {
-          provider = "litellm";
-          providers = {
-            litellm =
-              {
-                inherit model;
-                __inherited_from = "openai";
-                endpoint = "http://localhost:${port}/v1";
-                api_key_name = dummyLiteLLMApiKey;
-              };
-          };
-        };
-      };
+  programs.nixvim.plugins.avante = {
+    enable = true;
+
+    settings = {
+      provider = defaultProvider;
+
+      providers = providers;
     };
   };
 }
